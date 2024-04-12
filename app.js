1a1579825586ff92422ad49ff0e369a8634a479f
GuiPlante <166039674+GuiPlante@users.noreply.github.com>
2024-04-12 19:23:44 -0400

    app.js

--- app.js
+++ app.js
@@ -26,3 +26,30 @@ app.post("/update", jsonParser, (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 })
+
+app.post("/updatePurchaseOrder", jsonParser, (req, res) => {
+  console.log("Updating the online inventory system");
+  const body = JSON.stringify(req.body);
+
+  fs.readFile("./public/purchaseOrderDB.json", (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send("Error reading purchase order data");
+          return;
+      }
+
+      let purchaseOrders = JSON.parse(data);
+      purchaseOrders.push(req.body);
+
+      fs.writeFile("./public/purchaseOrderDB.json", JSON.stringify(purchaseOrders), (error) => {
+          if (error) {
+              console.error(error);
+              res.status(500).send("Error writing purchase order data");
+              return;
+          }
+
+          console.log("purchaseOrderDB.json updated successfully");
+          res.status(200).send("Purchase order added successfully");
+      });
+  });
+});
